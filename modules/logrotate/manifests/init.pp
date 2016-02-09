c62525af9e7e30607f72ac95d9c93f37bce0eb24
kyle <github@kylemiller.com>
2016-02-08 18:38:13 -0600

    added logroate discrete file refs

--- modules/logrotate/manifests/init.pp
+++ modules/logrotate/manifests/init.pp
@@ -7,11 +7,17 @@ class logrotate {
         ensure  => file,
         content => template("logrotate/unicorn_rails.erb"),
         require => Package['logrotate'],
-        #notify  => Service['logrotate'],
     }
 
-    #service { 'nginx':
-        #ensure  => running,
-        #enable  => true,
-    #}
+    file { '/etc/logrotate.d/clockworkd':
+        ensure  => file,
+        content => template("logrotate/clockworkd.erb"),
+        require => Package['logrotate'],
+    }
+
+    file { '/etc/logrotate.d/delayed_job':
+        ensure  => file,
+        content => template("logrotate/delayed_job.erb"),
+        require => Package['logrotate'],
+    }
 }
