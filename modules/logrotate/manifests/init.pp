2079bdbb06c1538f747beb9cdb076cc61d5252f2
kyle <github@kylemiller.com>
2016-02-08 18:00:52 -0600

    XX removed logrotate conf absent rule

--- modules/logrotate/manifests/init.pp
+++ modules/logrotate/manifests/init.pp
@@ -3,12 +3,6 @@ class logrotate {
         ensure  => installed,
     }
 
-    file { "/etc/logrotate.conf":
-      ensure  => absent,
-      require => Package['logrotate'],
-      #notify  => Service['logrotate'],
-    }
-
     file { '/etc/logrotate.d/unicorn_rails':
         ensure  => file,
         content => template("logrotate/unicorn_rails.erb"),
